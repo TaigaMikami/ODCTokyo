d4b98e76530c674857cfdec009b79237c2b3c687
TaigaMikami <mikamimikami60@gmail.com>
2018-03-05 19:51:54 +0900

    google map連携

---
+++ app/assets/javascripts/map/Geocoder.js
@@ -0,0 +1,24 @@
+// function initMap() {
+//   'use strict';
+//     var target = document.getElementById('map-target');
+//     var map;
+//      var tokyo = {lat: 35.681167, lng: 139.767052};
+//
+//      map = new google.maps.Map(target, {
+//       center: tokyo,
+//       zoom: 15,
+//       disableDefaultUI: true
+//      });
+//
+//      marker = new google.maps.Marker({
+//         position: tokyo,
+//         map: map,
+//         title: 'Tokyo!',
+//         // icon: 'icon.png',
+//         icon: {
+//           url: 'icon.png',
+//           scaledSize: new google.maps.Size(24, 24)
+//         },
+//         animation: google.maps.Animation.BOUNCE
+//       });
+// }
